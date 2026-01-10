034ef7ab5bfa59df8a983b55ca6b1e66b82575dc
FilipRegenczuk <f.regenczuk@outlook.com>
2026-01-10 17:14:34 +0100

    format

--- models/int_orders__pivoted.sql
+++ models/int_orders__pivoted.sql
@@ -1,3 +1,5 @@
+{% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
 with payments as (
     select * from {{ ref('stg_stripe__payments')}}
     where payment_status = 'success'
@@ -6,9 +8,11 @@ with payments as (
 pivoted as (
     select
         order_id,
-        {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
-        {% for method in payment_methods %}
-            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount,
+        {%- for method in payment_methods -%}
+            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+            {%- if not loop.last -%}
+                ,
+            {%- endif %}
         {% endfor %}
     from payments
     group by order_id
