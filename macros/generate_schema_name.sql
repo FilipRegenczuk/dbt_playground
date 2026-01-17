df62253009cfd548e6d9d7f83c3c21fd5674c245
FilipRegenczuk <f.regenczuk@outlook.com>
2026-01-17 13:13:32 +0100

    make schema target dependent

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {%- set target_name = target.name -%}
+
+    {%- if custom_schema_name is none or target_name == 'dev' -%}
 
         {{ default_schema }}
 
