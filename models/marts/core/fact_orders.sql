8ffd4e01142c9096aace5967d2e8942cd34d65ef
FilipRegenczuk <f.regenczuk@outlook.com>
2026-01-21 22:43:25 +0100

    add incremental model

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -1,3 +1,11 @@
+{{ 
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='order_id'
+    )
+}}
+
 with orders as  (
     select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
@@ -19,3 +27,7 @@ final as (
 )
 
 select * from final
+{% if is_incremental() %}
+    where order_date > (select max(order_date) from {{ this }})
+{% endif %}
+order by order_date desc
