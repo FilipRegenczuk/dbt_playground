3192b01c8f3991305767a9d9d536dd236a5787fe
FilipRegenczuk <f.regenczuk@outlook.com>
2026-01-21 22:43:05 +0100

    use window

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -12,6 +12,7 @@ renamed as (
         id as order_id,
         user_id as customer_id,
         order_date,
+        row_number() over (partition by user_id order by order_date) as order_number,
         status as order_status
 
     from source
