7d03253095011798632e1a0f0eef7c38111e04ed
FilipRegenczuk <f.regenczuk@outlook.com>
2026-02-01 11:16:04 +0100

    add v2 model

---
+++ models/marts/core/fact_orders_v2.sql
@@ -0,0 +1,32 @@
+{{ 
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='order_id'
+    )
+}}
+
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        payments.payment_status,
+        cast(orders.order_date as varchar) as order_date,
+    from orders
+    left join payments using (order_id)
+)
+
+select * from final
+{% if is_incremental() %}
+    where order_date > (select max(order_date) from {{ this }})
+{% endif %}
+order by order_date desc
